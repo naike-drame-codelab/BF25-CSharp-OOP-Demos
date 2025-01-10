70b9de3625dde14ab93ce30d41cd3e3a2255d117
naike.drame-codelab <naike.drame.dev@gmail.com>
2025-01-10 09:58:28 +0100

    added demo01 introduction and accessibility level

--- Demonstrations/Demo01_ClassProp/Models/Car.cs
+++ Demonstrations/Demo01_ClassProp/Models/Car.cs
@@ -7,10 +7,15 @@ using System.Threading.Tasks;
 namespace Demo01_ClassProp.Models
 {
     // par défaut, les éléments au sein d'un namespace non spécifiés sont internal
+    // internal : limité à son assembly/au projet où l'élément est défini
+
+    // enum : internal par défaut
+    // si je déclare une variable de type T, il faut que ce type T soit public également
+    enum Values { Value1, Value2, Value3 }
 
     class Car
     {
-        int Test {  get; set; }
+        int Test { get; set; }
 
         public string Color { get; set; }
     }
--- Demonstrations/Demo01_ClassProp/Program.cs
+++ Demonstrations/Demo01_ClassProp/Program.cs
@@ -3,9 +3,10 @@
 
 // ALT + ENTER : permet d'ajouter le using nécessaire à l'utilisation de la class
 // F12 sur l'objet : accès direct à la class
-Car c =  new Car();
+Car c = new Car();
 
 // par défaut, les élément d'une class ont un niveau d'accès private
 // c.Test
 
+// accès à une propriété d'une class et 
 c.Color = "Red";
