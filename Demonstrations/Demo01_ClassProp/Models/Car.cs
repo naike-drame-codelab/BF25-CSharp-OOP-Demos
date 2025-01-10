29c94bc9a6ed06bf54d99613aa55be89e0babf8a
naike.drame-codelab <naike.drame.dev@gmail.com>
2025-01-10 09:57:40 +0100

    added demo01 introduction & accessibility level

---
+++ Demonstrations/Demo01_ClassProp/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01_ClassProp.Models
+{
+    // par défaut, les éléments au sein d'un namespace non spécifiés sont internal
+
+    class Car
+    {
+        int Test {  get; set; }
+
+        public string Color { get; set; }
+    }
+}
---
+++ Demonstrations/Demo01_ClassProp/Program.cs
@@ -0,0 +1,11 @@
+﻿using Demo01_ClassProp.Models;
+
+
+// ALT + ENTER : permet d'ajouter le using nécessaire à l'utilisation de la class
+// F12 sur l'objet : accès direct à la class
+Car c =  new Car();
+
+// par défaut, les élément d'une class ont un niveau d'accès private
+// c.Test
+
+c.Color = "Red";
