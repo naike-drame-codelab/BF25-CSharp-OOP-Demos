8633c8a16b8d646053b6f859b5d305713f0a9719
Naïké Dramé <naike.drame.dev@gmail.com>
2025-01-16 15:59:33 +0100

    added demo generics

---
+++ Demonstrations/Demo11_Generiques/Program.cs
@@ -0,0 +1,35 @@
+﻿// Génériques : 
+// quand on ne sait pas encore avec quoi on va travailler, on va utiliser type T
+// en fonction du type du paramètre, le système va déterminer le type à attribuer
+// applicable sur des class ou des méthodes
+
+List<string> l = ["papa", "maman", "toto", "pepette"];
+List<string> result = l.Filter();
+
+List<T> Filter<T>(List<T> entree, Filtre<T> f)
+{
+    List<T> sortie = new List<T>();
+    foreach (T i in entree)
+    {
+        if (f.Invoke(i)) sortie.Add(i);
+    }
+
+    return sortie;
+}
+
+// méthodes qui sont passées aux delegates correspondants
+bool EstPair(int nb)
+{
+    return nb % 2 == 0;
+}
+bool EstPlusQue5(int nb)
+{
+    return nb > 5;
+}
+
+
+// création d'un type delegate
+// on ne peut enregistrer à l'intérieur d'un delegate que des fonctions void sans paramètres
+delegate void TypeDeFonction();
+
+delegate bool Filtre<T>(T nb);
