87a3a54964c4c0b4daeb8307b497dfda6172b25c
Naïké Dramé <naike.drame.dev@gmail.com>
2025-01-16 11:58:27 +0100

    added demo delegates

---
+++ Demonstrations/Demo08_Exceptions/Demo08_Exceptions/Program.cs
@@ -0,0 +1,57 @@
+﻿// capturer une exception
+
+// selon les applications, on va gérer les exceptions différemment
+// il incombe au --programme-- de savoir comment gérer l'exception, pas à la fonction
+// ce n'est pas que l'envoi de messages en cw, on peut envoyer un mail, un sms, créer un popup, faire un login, plusieurs éléments à la fois pour notifier de l'erreur capturée
+
+// rmq : le C# autorise la division par zéro pour les doubles (affiche symbole de l'infini) - double.PositiveInfinity, double.NegativeInfinity
+try
+{
+	int nb = int.Parse(Console.ReadLine());
+    Console.WriteLine(Diviser(42, nb));
+}
+// capture erreur de format
+catch (FormatException)
+{
+    Console.WriteLine("La valeur n'est pas un entier valide.");
+}
+// capture erreur null
+catch (ArgumentNullException)
+{
+    Console.WriteLine("La valeur ne peut pas être nulle.");
+}
+// capture d'une exception personnalisée
+catch (DivideByZeroException ex)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(ex.Message);
+    Console.ResetColor();
+}
+// capture tout autre exception
+catch (Exception)
+{
+    Console.WriteLine("Erreur inconnue.");
+}
+// que mon code ait échoué ou non, le programme va exécuter le bloc finally
+// https://learn.microsoft.com/fr-fr/dotnet/standard/exceptions/how-to-use-finally-blocks
+finally
+{
+    Console.WriteLine("Au revoir !!!");
+}
+
+
+double Diviser(double n1, double n2)
+{
+    if (n2 == 0)
+    {
+        // lancer une exception
+        throw new DivideByZeroException();
+    }
+    return n1 / n2;
+}
+
+// création d'une exception personnalisée, qui hérite de Exception
+class DivideByZeroException : Exception
+{
+    public DivideByZeroException() : base("Division par zéro impossible.") { }
+}
---
+++ Demonstrations/Demo09_Delegates/Program.cs
@@ -0,0 +1,102 @@
+﻿// fonctions exécutées selon l'ordre d'insertion
+using System.Net.NetworkInformation;
+
+// initialisation
+TypeDeFonction maVariable = DireBye;
+
+// ajout d'une nouvelle fonction avec l'opérateur +=
+maVariable += DireBonjour;
+
+// appel de la fonction // addEventListener()
+maVariable?.Invoke(); // 1) Bye ! 2) Hello !
+maVariable?.Invoke(); // 1) Bye ! 2) Hello !
+
+// suppression d'une fonction avec l'opérateur -=
+maVariable -= DireBye; 
+maVariable?.Invoke(); // Hello !
+
+// pas de ?. = erreur car plus de méthode à invoquer => UTILISATION DU NULL SAFE OPERATOR ?.
+maVariable -= DireBonjour;
+maVariable?.Invoke();
+
+List<int> list = [1,2,3,4,5,6,42,47,99];
+List<int> r1 =Filter(list, EstPair);
+List<int> r2= Filter(list, nb => nb % 3 == 0);
+
+
+// équivalent du Where dans LINQ
+List<int> r3= list.Where( nb => nb % 3 == 0).ToList();
+
+Console.WriteLine(string.Join(",", r1));
+
+//Filtre f1 = (nb) => nb % 3 == 0;
+//Filtre f2 = EstPair;
+//Filtre f3 = EstPlusQue5;
+
+void DireBonjour()
+{
+    Console.WriteLine("Hello !");
+}
+void DireBye()
+{
+    Console.WriteLine("Bye !");
+}
+
+int Somme (int nb1, int nb2)
+{
+    return nb1 + nb2;
+}
+
+#region Replacés par notre Filtrer()
+/* 
+ List<int> FilterPairs(List<int> entree)
+{
+    List<int> sortie = new List<int>();
+    foreach (int i in entree)
+    {
+        if (EstPair(i)) sortie.Add(i);
+    }
+
+    return sortie;
+}
+List<int> FilterPlusQue5(List<int> entree)
+{
+    List<int> sortie = new List<int>();
+    foreach (int i in entree)
+    {
+        if (EstPlusQue5(i)) sortie.Add(i);
+    }
+
+    return sortie;
+}
+*/
+#endregion
+
+
+List<int> Filter(List<int> entree, Filtre f)
+{
+    List<int> sortie = new List<int>();
+    foreach (int i in entree)
+    {
+        if (f.Invoke(i)) sortie.Add(i);
+    }
+
+    return sortie;
+}
+
+// méthodes qui sont passées aux delegates correspondants
+bool EstPair(int nb)
+{
+    return nb % 2 == 0;
+}
+bool EstPlusQue5(int nb)
+{
+    return nb > 5;
+}
+
+
+// création d'un type delegate
+// on ne peut enregistrer à l'intérieur d'un delegate que des fonctions void sans paramètres
+delegate void TypeDeFonction();
+
+delegate bool Filtre(int nb);
